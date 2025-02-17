681c0f1da52d6a04ee911a2851dda81e1f60e907
Celeste Hayes <celestehayes@Mac.attlocal.net>
2025-02-17 17:01:06 -0500

    Refactor: updates to auth.ts file to work with GraphQL

--- Develop/server/src/services/auth.ts
+++ Develop/server/src/services/auth.ts
@@ -1,39 +1,44 @@
-import type { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
-
+import { GraphQLError } from 'graphql';
 import dotenv from 'dotenv';
 dotenv.config();
 
-interface JwtPayload {
-  _id: unknown;
-  username: string;
-  email: string,
-}
+export const authenticateToken = ({ req }: any) => {
+  let token = req.body.token || req.query.token || req.headers.authorization;
 
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-  const authHeader = req.headers.authorization;
-
-  if (authHeader) {
-    const token = authHeader.split(' ')[1];
-
-    const secretKey = process.env.JWT_SECRET_KEY || '';
-
-    jwt.verify(token, secretKey, (err, user) => {
-      if (err) {
-        return res.sendStatus(403); // Forbidden
-      }
-
-      req.user = user as JwtPayload;
-      return next();
-    });
-  } else {
-    res.sendStatus(401);
+  // Extract token from the auth header
+  if (req.headers.authorization) {
+    token = token.split(' ').pop().trim();
   }
+  // If no token is provided, return request object as is
+  if (!token) {
+    return req;
+  }
+  // Try to verify token
+  try {
+    const { data }: any = jwt.verify(token, process.env.JWT_SECRET_KEY || '', { maxAge: '2hr' });
+    // If token is valid, attach the user data to the request object
+    req.user = data;
+  } catch (err) {
+    // If token is invalid, log an error message
+    console.log('Invalid token');
+  }
+  // Return request object
+  return req;
 };
 
 export const signToken = (username: string, email: string, _id: unknown) => {
+  // Create a payload with user info
   const payload = { username, email, _id };
-  const secretKey = process.env.JWT_SECRET_KEY || '';
+  const secretKey: any = process.env.JWT_SECRET_KEY;
 
-  return jwt.sign(payload, secretKey, { expiresIn: '1h' });
+  // Sign token with payload and secret key
+  return jwt.sign({ data: payload }, secretKey, { expiresIn: '2h' });
+};
+
+export class AuthenticationError extends GraphQLError {
+  constructor(message: string) {
+    super(message, undefined, undefined, undefined, ['UNAUTHENTICATED']);
+    Object.defineProperty(this, 'name', { value: 'AuthenticationError' });
+  }
 };
